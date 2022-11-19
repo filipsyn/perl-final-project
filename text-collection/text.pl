9ed5de260713a69e03d8158db467557002caf222
Filip Synek <xjust@mendelu.cz>
2022-11-19 18:47:55 +0100

    Added command-line flag parsing and fixed bug
    - Added missing semicolon after $local_weight declaration.
    - Added command-line flag parsing using GetOptions funcion.

--- text-collection/text.pl
+++ text-collection/text.pl
@@ -4,6 +4,15 @@ use Getopt::Long;
 $input_file = "input.txt";
 $minimal_length = 1;
 $minimal_occurance = 1;
-$local_weight = "tp"
+$local_weight = "tp";
+
+# Parsing command line arguments to corresponding variables.
+GetOptions( "input|i=s" => \$input_file,
+            "minimal-length|l=i" => \$minimal_length,
+            "minimal-occurance|n=i" => \$minimal_occurance,
+            "local-weight|w=s" => \$local_weight
+) or die "Error in command line arguments\n";
+
+print "$input_file $minimal_length $minimal_occurance $local_weight\n";
 
 print "Hello world", "\n";
