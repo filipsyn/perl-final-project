fa44e0eab6a3d7a8a5a346259eac975699e4cade
Filip Synek <xjust@mendelu.cz>
2022-11-19 18:55:52 +0100

    Added strict mode and enforcing valid values of local_weight
    - The script now uses strict mode
    - Added check if $local_weight is either "tp" or "tf"

--- text-collection/text.pl
+++ text-collection/text.pl
@@ -1,10 +1,11 @@
 use Getopt::Long;
+use strict;
 
 # Default values for arguments
-$input_file = "input.txt";
-$minimal_length = 1;
-$minimal_occurance = 1;
-$local_weight = "tp";
+our $input_file = "input.txt";
+our $minimal_length = 1;
+our $minimal_occurance = 1;
+our $local_weight = "tp";
 
 # Parsing command line arguments to corresponding variables.
 GetOptions( "input|i=s" => \$input_file,
@@ -12,6 +13,9 @@ GetOptions( "input|i=s" => \$input_file,
             "minimal-occurance|n=i" => \$minimal_occurance,
             "local-weight|w=s" => \$local_weight
 ) or die "Error in command line arguments\n";
+
+# Checking valid if local_weight has eiher "tp" or "tf" value
+die "Invalid local weight value provided.\nPlease provide either 'tp' (default) or 'tf' value\n" unless ($local_weight eq "tp" or $local_weight eq "tf");
 
 print "$input_file $minimal_length $minimal_occurance $local_weight\n";
 
