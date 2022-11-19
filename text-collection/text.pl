8f6e98f9984bad9f564ccd00b08f991cfe4c28d6
Filip Synek <xjust@mendelu.cz>
2022-11-19 19:10:41 +0100

    Added file opening and adjusted command-line flags.

--- text-collection/text.pl
+++ text-collection/text.pl
@@ -8,7 +8,7 @@ our $minimal_occurance = 1;
 our $local_weight = "tp";
 
 # Parsing command line arguments to corresponding variables.
-GetOptions( "input|i=s" => \$input_file,
+GetOptions( "input-file|f=s" => \$input_file,
             "minimal-length|l=i" => \$minimal_length,
             "minimal-occurance|n=i" => \$minimal_occurance,
             "local-weight|w=s" => \$local_weight
@@ -17,6 +17,5 @@ GetOptions( "input|i=s" => \$input_file,
 # Checking valid if local_weight has eiher "tp" or "tf" value
 die "Invalid local weight value provided.\nPlease provide either 'tp' (default) or 'tf' value\n" unless ($local_weight eq "tp" or $local_weight eq "tf");
 
-print "$input_file $minimal_length $minimal_occurance $local_weight\n";
-
-print "Hello world", "\n";
+# Opening input file
+open F, "$input_file" or die "Can't open file $input_file\n";
