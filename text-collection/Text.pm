35bbd19c17e5e04c48f27a461e7faf21a335c2f3
Filip Synek <xjust@mendelu.cz>
2022-11-25 14:01:04 +0100

    Helper function put into separate module

---
+++ text-collection/Text.pm
@@ -0,0 +1,25 @@
+package Text;
+
+use Exporter;
+@ISA = qw ('Exporter');
+@EXPORT = qw ('strip_text');
+@EXPORT_OK = qw(strip_text);
+
+
+sub strip_text {
+    # Stripes HTML tags and non-letter characters from text.
+    # Returns cleaned text in uppercase, to standardize the text.
+
+    # Takes "dirty" text as only argument
+    my $text = shift;
+
+    # Striping out HTML tags
+    $text =~ s/(<([^>]+)>)//g;
+
+    # Removing any non-letter character
+    $text =~ s/[^[:alpha:][:space:]]//g;
+
+    return uc $text;
+}
+
+1;
