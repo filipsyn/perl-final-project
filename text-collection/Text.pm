51adf4e69a2cf14786a74405d3d14b6a5f7ef001
Filip Synek <xjust@mendelu.cz>
2022-11-26 18:54:38 +0100

    Implemented function to calculate decadic logarithm of number

--- text-collection/Text.pm
+++ text-collection/Text.pm
@@ -37,4 +37,13 @@ sub in_array {
     return 0;
 }
 
+sub log10 {
+    # Subrutine to calculate decadic logarihm
+    # Perl's standard log() function uses e as a base 
+    # This code is taken from Perl Func documentation
+    # https://perldoc.perl.org/perlfunc#log
+    my $n = shift;
+    return log($n)/log(10);
+}
+
 1;
