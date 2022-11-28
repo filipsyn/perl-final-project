c1bf16bd27d47a52d5c92c5f8b8dada7be404f79
Filip Synek <xjust@mendelu.cz>
2022-11-28 16:22:23 +0100

    Added other global structures

--- graph-activation/main.pl
+++ graph-activation/main.pl
@@ -21,3 +21,18 @@ our %nodes;
 #   }
 our @links;
 
+# Beta parameter
+# Used for calculating value sent to other nodes
+our $beta;
+
+# Number of iterations that algorithm runs for
+our $iterations_limit;
+
+# Decides what type of calibration is used to normalize node values
+our $calibration_type;
+
+# Parameters hash
+# Parameters a, b and c are used to calculate new values of nodes
+our %param = (a => 0, b => 0, c => 0);
+
+
