0821ef41e83d6e5d02269f91fcb14bcaa0429035
Filip Synek <xjust@mendelu.cz>
2022-12-06 19:51:07 +0100

    `log10` function declaration moved into main source file.

--- text-collection/Text.pm
+++ text-collection/Text.pm
@@ -6,14 +6,4 @@ use Exporter;
 @EXPORT = qw(log10);
 
 
-sub log10 {
-
-    # Subrutine to calculate decadic logarihm
-    # Perl's standard log() function uses e as a base.
-    # This code is taken from Perl Func documentation
-    # https://perldoc.perl.org/perlfunc#log
-    my $n = shift;
-    return log($n) / log(10);
-}
-
 1;
--- text-collection/main.pl
+++ text-collection/main.pl
@@ -42,6 +42,15 @@ sub strip_text {
     return uc $text;
 }
 
+sub log10 {
+    # Subrutine to calculate decadic logarihm
+    # Perl's standard log() function uses e as a base.
+    # This code is taken from Perl Func documentation
+    # https://perldoc.perl.org/perlfunc#log
+    my $n = shift;
+    return log($n) / log(10);
+}
+
 sub term_occurance_in_documents {
     # Returns number of documents in which searched term occures
     my $term = uc shift;
